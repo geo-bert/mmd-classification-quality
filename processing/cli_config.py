0092be8921c66c3c538456c620b646af823bd046
geo-bert <grybl.business@gmail.com>
2022-04-11 20:27:40 +0200

    Pythonify compression_singles

--- processing/cli_config.py
+++ processing/cli_config.py
@@ -1,16 +1,15 @@
 """Config to personalize cli command locations"""
 """JPEGXL"""
-cjxl = "/home/matthias/libjxl/libjxl/build/tools/cjxl"
-djxl = "/home/matthias/libjxl/libjxl/build/tools/djxl"
+cjxl = "cjxl"
+djxl = "djxl"
 
 """WEBP"""
 cwebp = "cwebp"
 dwebp = "dwebp"
 
-
 """JXR"""
 JxrEncApp = "JxrEncApp"
-JxrDecApp ="JxrDecApp"
+JxrDecApp = "JxrDecApp"
 
 """imagemagick"""
-magick="convert"
+magick = "convert"
--- processing/compression_singles.py
+++ processing/compression_singles.py
@@ -1,53 +1,60 @@
 import os
 from posixpath import splitext
+
 import cli_config
 
 
-def single_compression_xl(image: str,out_dir: str, quality: int) -> str:
+def single_compression_xl(image: str, out_dir: str, quality: int) -> str:
     image_out = create_out_image_path(image, out_dir, "jxl")
-    print(image_out)
-    print(cli_config.cjxl +" "+image+" "+ image_out +" -q " +str(quality))
-    os.system(cli_config.cjxl +" "+image+" "+ image_out +" -q " +str(quality))
+    os.system(f"{cli_config.cjxl} {image} {image_out} -q {quality}")
     return image_out
 
-def single_compression_webp(image: str,out_dir: str, quality: float) -> str:
-    image_out = create_out_image_path(image,out_dir, "webp")
-    os.system(cli_config.cwebp +" -q "+str(quality)+" "+image+" -o "+image_out)
+
+def single_compression_webp(image: str, out_dir: str, quality: float) -> str:
+    image_out = create_out_image_path(image, out_dir, "webp")
+    os.system(f"{cli_config.cwebp} -q {quality} {image} -o {image_out}")
     return image_out
 
-def single_compression_xr(image: str,out_dir: str, quality: float) -> str:
-    image_out = create_out_image_path(image,out_dir, "jxr")
-    os.system(cli_config.JxrEncApp +" -q " + str(quality)+" -i "+image+" -o "+image_out)
+
+def single_compression_xr(image: str, out_dir: str, quality: float) -> str:
+    image_out = create_out_image_path(image, out_dir, "jxr")
+    os.system(f"{cli_config.JxrEncApp} -q {quality} -i {image} -o {image_out}")
     return image_out
 
-def single_compression_jpg(image: str,out_dir: str, quality: int) -> str:
-    image_out = create_out_image_path(image,out_dir, "jpg")
-    os.system(cli_config.magick+" -quality "+ str(quality)+" "+image+" "+image_out)
-    return image_out    
 
-def single_compression_heic(image: str,out_dir: str, quality: int) -> str:
-    image_out = create_out_image_path(image,out_dir, "heic")
-    os.system(cli_config.magick+" "+image+" -quality "+str(quality)+" "+image_out)
-    return image_out   
+def single_compression_jpg(image: str, out_dir: str, quality: int) -> str:
+    image_out = create_out_image_path(image, out_dir, "jpg")
+    os.system(f"{cli_config.magick} -quality {quality} {image} {image_out}")
+    return image_out
 
-def single_compression_jp2(image: str,out_dir: str, quality: int) -> str:
-    image_out = create_out_image_path(image,out_dir, "jp2")
-    os.system(cli_config.magick+" "+image+" -quality "+str(quality)+" "+image_out)
-    return image_out  
 
-def create_out_image_path(image_in: str, out_dir: str, format: str) -> str:
+def single_compression_heic(image: str, out_dir: str, quality: int) -> str:
+    image_out = create_out_image_path(image, out_dir, "heic")
+    os.system(f"{cli_config.magick} {image} -quality {quality} {image_out}")
+    return image_out
+
+
+def single_compression_jp2(image: str, out_dir: str, quality: int) -> str:
+    image_out = create_out_image_path(image, out_dir, "jp2")
+    os.system(f"{cli_config.magick} {image} -quality {quality} {image_out}")
+    return image_out
+
+
+def create_out_image_path(image_in: str, out_dir: str, extension: str) -> str:
     image_base_name = splitext(os.path.split(image_in)[1])[0]
-    image_out = os.path.join(out_dir, image_base_name+"."+format)
+    image_out = os.path.join(out_dir, f"{image_base_name}.{extension}")
     return image_out
 
-"""for testing"""
-def compressDir(filepath: str): 
+
+def compress_dir(filepath: str):
+    """for testing"""
     quality = 20
     original_images = os.path.join(filepath, "original")
-    compressed_images = os.path.join(filepath,"comp_jp2_"+str(quality))
+    compressed_images = os.path.join(filepath, f"comp_jp2_{quality}")
     os.mkdir(compressed_images)
     for f in os.listdir(original_images):
-        single_compression_jp2(os.path.join(original_images,f), compressed_images, quality)
+        single_compression_jp2(os.path.join(original_images, f), compressed_images, quality)
 
 
-compressDir("../example_images")
+if __name__ == "__main__":
+    compress_dir("../example_images")
