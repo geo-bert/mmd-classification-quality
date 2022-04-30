1556c443c626663b40d43522de59232de03d23ef
geo-bert <grybl.business@gmail.com>
2022-04-30 15:27:19 +0200

    Remove superfluous files

--- processing/test.py
+++
@@ -1,8 +0,0 @@
-import quality_metrics as qm
-
-ref_path = "../example_images/original/Buildings.0001.png"
-vgl_path = "../example_images/decompressed/Buildings.001_jpg.png"
-
-print(qm.psnr(vgl_path, ref_path))
-print(qm.mssim(vgl_path, ref_path))
-print(qm.niqe(vgl_path))
