9fc4466e201dc7e287d76b97e5ee056e2c521ff2
geo-bert <grybl.business@gmail.com>
2022-04-30 16:35:08 +0200

    Change paths to actual ones

--- classifier/src/main.py
+++ classifier/src/main.py
@@ -7,37 +7,43 @@ from config_generator import generate_configs
 
 KNN_K = 1
 
+
 def run_experiment(paths: list[str], result_file: str):
-  configs = generate_configs()
-  print(f"Starting experiment for {len(configs)} configurations")
-  for config in configs:
-    feat_vecs = get_feature_vectors(paths, config)
-    total_accuracy = run_classification(feat_vecs, KNN_K)
-    _store_results(config, total_accuracy, result_file)
+    configs = generate_configs()
+    print(f"Starting experiment for {len(configs)} configurations")
+    for config in configs:
+        feat_vecs = get_feature_vectors(paths, config)
+        total_accuracy = run_classification(feat_vecs, KNN_K)
+        _store_results(config, total_accuracy, result_file)
 
 
 def _store_results(config: FeatureConfig, result: float, file: str):
-  line = f"{result},{config.block_size},{config.color_channel},{config.bin_width},{config.dct_coefficients}\n"
+    line = f"{result},{config.block_size},{config.color_channel},{config.bin_width},{config.dct_coefficients}\n"
 
-  if os.path.exists(file):
-    with open(file, "a") as f:
-      f.write(line)
-  else:
-    with open(file, "x") as f:
-      f.write(
-        "total_result,block_size,color_channel,bin_width,coefficients\n"
-      )
-      f.write(line)
+    if os.path.exists(file):
+        with open(file, "a") as f:
+            f.write(line)
+    else:
+        with open(file, "x") as f:
+            f.write(
+                "total_result,block_size,color_channel,bin_width,coefficients\n"
+            )
+            f.write(line)
 
 
 def main():
-  base_path = "example_images"
-  paths = [
-    os.path.join(base_path, "decompressed"),
-    os.path.join(base_path, "original")
-  ]
-  run_experiment(paths, "experiment_results.csv")
+    base_path = "images/output/psnr"
+    paths = [
+        os.path.join(base_path, "heic"),
+        os.path.join(base_path, "jp2"),
+        os.path.join(base_path, "jpg"),
+        os.path.join(base_path, "jxl"),
+        os.path.join(base_path, "jxr"),
+        os.path.join(base_path, "webp")
+
+    ]
+    run_experiment(paths, "experiment_results.csv")
 
 
 if __name__ == '__main__':
-  main()
+    main()
