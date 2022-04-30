3c22fdc8940bb955ec2e929e110922e3b344a611
geo-bert <grybl.business@gmail.com>
2022-04-30 15:31:16 +0200

    Convert to PNG first, silence output

--- processing/compress.py
+++ processing/compress.py
@@ -5,44 +5,50 @@ from posixpath import splitext
 
 def jxl(image: str, out_dir: str, quality: int):
     image_out = _create_out_image_path(image, out_dir, "jxl")
-    os.system(f"cjxl {image} {image_out} -q {quality}")
+    os.system(f"magick {image} {image[:-4]}.png")
+    os.system(f"cjxl {image[:-4]}.png {image_out} -q {quality} >/dev/null 2>&1")
     decompress.jxl(image_out)
-    return image_out
+    os.system(f"rm {image[:-4]}.png")
+    return f"{image_out}.png"
 
 
 def webp(image: str, out_dir: str, quality: float):
     image_out = _create_out_image_path(image, out_dir, "webp")
-    os.system(f"cwebp -q {quality} {image} -o {image_out}")
+    os.system(f"magick {image} {image[:-4]}.png")
+    os.system(f"cwebp -q {quality} {image[:-4]}.png -o {image_out} >/dev/null 2>&1")
     decompress.webp(image_out)
-    return image_out
+    os.system(f"rm {image[:-4]}.png")
+    return f"{image_out}.png"
 
 
 def jxr(image: str, out_dir: str, quality: int):
     image_out = _create_out_image_path(image, out_dir, "jxr")
-    os.system(f"JxrEncApp -q {quality / 100} -i {image} -o {image_out}")
+    os.system(f"magick {image} {image[:-4]}.tif")
+    os.system(f"JxrEncApp -q {quality / 100} -i {image[:-4]}.tif -o {image_out} >/dev/null 2>&1")
     decompress.jxr(image_out)
-    return image_out
+    os.system(f"rm {image[:-4]}.tif")
+    return f"{image_out}.png"
 
 
 def jpg(image: str, out_dir: str, quality: int):
     image_out = _create_out_image_path(image, out_dir, "jpg")
     os.system(f"magick -quality {quality} {image} {image_out}")
     decompress.jpg(image_out)
-    return image_out
+    return f"{image_out}.png"
 
 
 def heic(image: str, out_dir: str, quality: int):
     image_out = _create_out_image_path(image, out_dir, "heic")
-    os.system(f"heif-enc -q {quality} -o {image_out} {image}")
+    os.system(f"magick {image} -quality {quality} {image_out}")
     decompress.heic(image_out)
-    return image_out
+    return f"{image_out}.png"
 
 
 def jp2(image: str, out_dir: str, quality: int):
     image_out = _create_out_image_path(image, out_dir, "jp2")
     os.system(f"magick {image} -quality {quality} {image_out}")
     decompress.jp2(image_out)
-    return image_out
+    return f"{image_out}.png"
 
 
 def _create_out_image_path(image_in: str, out_dir: str, extension: str) -> str:
--- processing/decompress.py
+++ processing/decompress.py
@@ -2,31 +2,31 @@ import os
 
 
 def jpg(inp: str):
-    os.system(f"djpeg -outfile {inp}.ppm {inp}")
+    os.system(f"djpeg -outfile {inp}.ppm {inp} >/dev/null 2>&1")
     os.system(f"magick {inp}.ppm {inp}.png")
     os.system(f"rm {inp}")
     os.system(f"rm {inp}.ppm")
 
 
 def jxl(inp: str):
-    os.system(f"djxl {inp} {inp}.png")
+    os.system(f"djxl {inp} {inp}.png >/dev/null 2>&1")
     os.system(f"rm {inp}")
 
 
 def jp2(inp: str):
-    os.system(f"opj_decompress -i {inp} -o {inp}.png")
+    os.system(f"opj_decompress -i {inp} -o {inp}.png >/dev/null 2>&1")
     os.system(f"rm {inp}")
 
 
 def jxr(inp: str):
-    os.system(f"JxrDecApp -i {inp} -o {inp}.tif")
+    os.system(f"JxrDecApp -i {inp} -o {inp}.tif >/dev/null 2>&1")
     os.system(f"magick {inp}.tif {inp}.png")
     os.system(f"rm {inp}")
     os.system(f"rm {inp}.tif")
 
 
 def webp(inp: str):
-    os.system(f"dwebp {inp} -o {inp}.png")
+    os.system(f"dwebp {inp} -o {inp}.png >/dev/null 2>&1")
     os.system(f"rm {inp}")
 
 
