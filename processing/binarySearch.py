8489ed869529528d789fb2d8fd2c5e2e3dff4115
geo-bert <grybl.business@gmail.com>
2022-04-30 11:26:30 +0200

    Reformat processing files

--- processing/binarySearch.py
+++ processing/binarySearch.py
@@ -1,41 +1,31 @@
-
-def binary_search(arr_q_steps, orig_img, wanted_quality, compression_func,quality_func, out_dir):
+def binary_search(arr_q_steps, orig_img, wanted_quality, compression_func, quality_func, out_dir):
     low = 0
     high = len(arr_q_steps) - 1
     mid = 0
-    best_estimate = (-1,float('inf'))
-    
+    best_estimate = (-1, float('inf'))
+
     def calc_new_quality():
         new_image = compression_func(orig_img, out_dir, mid)
-        new_quality = quality_func(new_image,orig_img)
+        new_quality = quality_func(new_image, orig_img)
         print(f"calculated {new_quality} with quality option: {mid}")
-        return  (new_quality)
-
-
+        return new_quality
 
     while low <= high:
- 
+
         mid = (high + low) // 2
 
-        currQ = calc_new_quality()
-        
- 
-       
-        if currQ < wanted_quality:
-            low = mid + 1
- 
-        
-        elif currQ > wanted_quality:
-            high = mid - 1
- 
-        
-        else:
-            return (currQ,mid)
-        
-        # we need to approach quality from below and set new best estimate if
-        if currQ < wanted_quality and (wanted_quality - currQ) < (wanted_quality - best_estimate[0]):
-            best_estimate = (currQ,mid)  
- 
-    # return best estimate here because we will not find the quality spot on (not likely)
-    return best_estimate
+        curr_q = calc_new_quality()
 
+        if curr_q < wanted_quality:
+            low = mid + 1
+        elif curr_q > wanted_quality:
+            high = mid - 1
+        else:
+            return curr_q, mid
+
+        # we need to approach quality from below and set new best estimate if
+        if curr_q < wanted_quality and (wanted_quality - curr_q) < (wanted_quality - best_estimate[0]):
+            best_estimate = (curr_q, mid)
+
+            # return best estimate here because we will not find the quality spot on (not likely)
+    return best_estimate
--- processing/experiment.py
+++ processing/experiment.py
@@ -7,8 +7,7 @@ out_dir = "../test"
 watned_psnr = 42
 possible_quals = list(range(1, 100))
 
-result = binarySearch.binary_search(possible_quals, orig_image, watned_psnr, compression_singles.single_compression_jpg, quality_metrics.psnr, out_dir)
+result = binarySearch.binary_search(possible_quals, orig_image, watned_psnr, compression_singles.single_compression_jpg,
+                                    quality_metrics.psnr, out_dir)
 
 print(result)
-
-
