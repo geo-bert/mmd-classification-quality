427f06058812df7227d364578816881319607c5a
geo-bert <grybl.business@gmail.com>
2022-05-10 12:00:38 +0200

    Ignore approximation direction.

--- processing/binary_search.py
+++ processing/binary_search.py
@@ -16,14 +16,14 @@ def search(img_in, target, fn_compress, fn_quality, out, from_below=True):
         mid = (high + low) // 2
         q = calc_new_quality(mid)
 
-        if from_below and metric < q < target:
+        if from_below and abs(metric-target) < abs(q-target):
             metric = q
             quality_flag = mid
-        elif not from_below and metric > q > target:
+        elif not from_below and abs(metric-target) > abs(q-target):
             metric = q
             quality_flag = mid
 
-        if q < target:
+        if math.isnan(q) or q < target:
             binary_search(mid + 1, high)
         elif q > target:
             binary_search(low, mid - 1)
