43890ee1b4c47e707c833c3cd8c977b6517601cb
Manuel Hatzl <hatzlmanuel@outlook.com>
2022-05-11 15:32:14 +0200

    test: add niqe_6 tests with other configs

--- classifier/src/config_generator.py
+++ classifier/src/config_generator.py
@@ -4,17 +4,17 @@ from channel_enum import ChannelYUV
 from feature_config import FeatureConfig
 
 SMALLEST_BLOCK_SIZE = 6
-BIGGEST_BLOCK_SIZE = 14
+BIGGEST_BLOCK_SIZE = 8
 BLOCK_SIZE_INCREMENT = 2
 
-RANDOM_COEFF_ITERATION_PER_CONFIG = 5
-MAX_NR_ADDITIONAL_COEFFS_PER_CONFIG = 4  # excluding DC
+RANDOM_COEFF_ITERATION_PER_CONFIG = 12
+MAX_NR_ADDITIONAL_COEFFS_PER_CONFIG = 10  # excluding (1,3)
 
 MAX_NR_CONFIGS = 100
 
 
 def generate_configs(max_nr: int = MAX_NR_CONFIGS) -> list[FeatureConfig]:
-    base_config = FeatureConfig(ChannelYUV.Y, 7, 0.1, [(2, 2)])
+    base_config = FeatureConfig(ChannelYUV.V, 6, 0.1, [(1, 3)])
 
     configs = _prepare_multiple_block_sizes(base_config)
 
@@ -40,8 +40,8 @@ def _prepare_multiple_block_sizes(feature_config: FeatureConfig) -> list[Feature
 
 def _prepare_channels(feature_config: FeatureConfig) -> list[FeatureConfig]:
     configs: list[FeatureConfig] = [
-        FeatureConfig(ChannelYUV.Y, feature_config.block_size, feature_config.bin_width,
-                      feature_config.dct_coefficients),
+        # FeatureConfig(ChannelYUV.Y, feature_config.block_size, feature_config.bin_width,
+        #               feature_config.dct_coefficients),
         FeatureConfig(ChannelYUV.U, feature_config.block_size, feature_config.bin_width,
                       feature_config.dct_coefficients),
         FeatureConfig(ChannelYUV.V, feature_config.block_size, feature_config.bin_width,
@@ -60,12 +60,16 @@ def _prepare_random_coefficient_group(feature_config: FeatureConfig) -> list[Fea
             coefficients.append((i, j))
 
     for _ in range(RANDOM_COEFF_ITERATION_PER_CONFIG):
-        nr_of_coeffs = random.randint(1, min(feature_config.block_size, MAX_NR_ADDITIONAL_COEFFS_PER_CONFIG))
-        coeff_indizes_to_take: set[int] = {0}  # DC coeff is always used
+        nr_of_coeffs = random.randint(1, min(
+            feature_config.block_size * feature_config.block_size, 
+            MAX_NR_ADDITIONAL_COEFFS_PER_CONFIG
+        ))
+        coeff_indizes_to_take: set[int] = {0}  # DC coeff used to init set. Emptry {} does not create set
+        coeff_indizes_to_take.remove(0)
         for _ in range(nr_of_coeffs):
-            index = random.randint(0, len(coefficients) - 1)
+            index = random.randint(1, len(coefficients) - 1) # Exclude DC by starting with offset 1
             coeff_indizes_to_take.add(index)
-
+        
         coeffs_to_take = []
         for index in coeff_indizes_to_take:
             coeffs_to_take.append(coefficients[index])
