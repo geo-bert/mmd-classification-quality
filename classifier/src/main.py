ac79e577400aa9c53dbb826adba16cbbaed95365
Manuel Hatzl <hatzlmanuel@outlook.com>
2022-06-09 15:45:39 +0200

    test: add niqe 5.0 tests

--- classifier/src/main.py
+++ classifier/src/main.py
@@ -42,7 +42,7 @@ def _store_results(config: FeatureConfig, result: float, file: str):
 
 
 def main():
-    base_path = "images/output/niqe_5.5"
+    base_path = "images/output/niqe_5.0"
     paths = [
         os.path.join(base_path, "heic"),
         os.path.join(base_path, "jp2"),
@@ -52,12 +52,12 @@ def main():
         os.path.join(base_path, "webp")
 
     ]
-    # run_experiment(paths, "experiments/experiment_results_psnr33_5.csv")
+    run_experiment(paths, "experiments/experiment_results_niqe_5_0.csv")
 
     config = FeatureConfig(color_channel=ChannelYUV.V, block_size=6, bin_width=0.1, dct_coefficients=[
         (0,5),(1,1),(1,3),(1,5),(4,5),(5,0)
     ])
-    run_specific_experiment(paths, "experiments/experiment_results_niqe5_5_single.csv", config)
+    run_specific_experiment(paths, "experiments/experiment_results_niqe5_0_single.csv", config)
 
 
 if __name__ == '__main__':
